65841c37f1b92cded98b6b2b0abd24e6bfb67a93
Sergei Bessonov <s.bessonov@planeta.ru>
2026-05-28 16:40:52 +0300

    feat(elementCalculation): beautify

--- src/main/java/com/code/tasks/elementcalcualtion/ElementCalculator.java
+++ src/main/java/com/code/tasks/elementcalcualtion/ElementCalculator.java
@@ -5,8 +5,8 @@ import java.util.Map;
 
 public class ElementCalculator {
 
-    public static <K> Map<K,Long> getCountByElement(K[] array){
-        Map<K,Long> countByElements = new HashMap<>();
+    public static <K> Map<K, Long> getCountByElement(K[] array) {
+        Map<K, Long> countByElements = new HashMap<>();
         for (K key : array) {
             countByElements.merge(key, 1L, Long::sum);
         }
