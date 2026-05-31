7c679970cb1ec44b4848968849f3d2404668121e
Sergei Bessonov <s.bessonov@planeta.ru>
2026-05-31 20:27:44 +0300

    feat(concurrentBank): added concurrent bank class

--- src/main/java/com/code/Main.java
+++ src/main/java/com/code/Main.java
@@ -1,17 +1,39 @@
 package com.code;
 
+import com.code.tasks.threads.bank.BankAccount;
+import com.code.tasks.threads.bank.ConcurrentBank;
+
+import java.math.BigDecimal;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ConcurrentBank bank = new ConcurrentBank();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Создание счетов
+        BankAccount account1 = bank.createAccount(new BigDecimal("1000"));
+        BankAccount account2 = bank.createAccount(new BigDecimal("500"));
+
+        // Перевод между счетами
+        Thread transferThread1 = new Thread(() -> bank.transfer(account1, account2, new BigDecimal("200")));
+        Thread transferThread2 = new Thread(() -> bank.transfer(account2, account1, new BigDecimal("100")));
+
+        transferThread1.start();
+        transferThread2.start();
+
+        try {
+            transferThread1.join();
+            transferThread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+
+        // Вывод общего баланса
+        System.out.println("Total balance: " + bank.getTotalBalance());
+        // Вывод общего баланса
+        System.out.println("Total 1balance: " + account1.getBalance());
+        // Вывод общего баланса
+        System.out.println("Total 2balance: " + account2.getBalance());
     }
 }
