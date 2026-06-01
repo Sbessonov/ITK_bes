bff0fa94a6ee3ec9d95883f782f7a08f3476c139
Sergei Bessonov <s.bessonov@planeta.ru>
2026-06-01 17:43:50 +0300

    Revert "feat(gradeAggregation): added method to calculate average subject grade"
    This reverts commit ddf34e26409f70aec5e1e93c5d64140d72223a7e.

--- src/main/java/com/code/Main.java
+++ src/main/java/com/code/Main.java
@@ -1,22 +1,8 @@
 package com.code;
 
 
-import com.code.stream.parallel.ParallelStreamProcessor;
-import com.code.stream.parallel.Student;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-
 public class Main {
     public static void main(String[] args) {
 
-        List<Student> students = Arrays.asList(
-                new Student("Student1", Map.of("Math", 90, "Physics", 85)),
-                new Student("Student2", Map.of("Math", 95, "Physics", 88)),
-                new Student("Student3", Map.of("Math", 88, "Chemistry", 92)),
-                new Student("Student4", Map.of("Physics", 78, "Chemistry", 85))
-        );
-        ParallelStreamProcessor.getAverageGrades(students).entrySet().forEach(System.out::println);
     }
 }
